7ca1f6666a851797b4b5e1cb4cd96285eaf1f773
Silvia <silvia.ferrara@mail.polimi.it>
2023-03-28 17:28:48 +0200

    Added TileType enum

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/server/model/TileType.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.server.model;
+
+import java.io.Serializable;
+
+/**
+ * Enum containing the different types of tiles
+ */
+public enum TileType implements Serializable {
+    BOOK, CAT, FRAME, PLANT, TOY, TROPHY
+}
