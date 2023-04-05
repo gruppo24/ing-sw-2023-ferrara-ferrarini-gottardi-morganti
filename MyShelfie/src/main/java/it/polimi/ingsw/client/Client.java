9b4c12de5cf8241a28534c68ce19e20dd80a6339
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-04-05 11:34:47 +0200

    CLI prototype

--- MyShelfie/src/main/java/it/polimi/ingsw/client/Client.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/Client.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.client;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.UnknownHostException;
 
+import it.polimi.ingsw.client.view.CLI;
+
+public class Client {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        CLI cli = new CLI();
+        cli.menu();
     }
 }
