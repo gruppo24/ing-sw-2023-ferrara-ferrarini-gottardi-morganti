5679462cce794d34ffd7984669c958ad12a4ab15
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-04-12 12:09:59 +0200

    italic not pickable

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/CLIUtils.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/CLIUtils.java
@@ -8,6 +8,7 @@ public class CLIUtils {
 
     private static final String ANSI_BOLD = "\u001B[1m";
     private static final String ANSI_FAINT = "\u001B[2m";
+    private static final String ANSI_ITALIC = "\u001B[3m";
     private static final String ANSI_UNDERLINE = "\u001B[4m";
 
     private static final String ANSI_CLEAR_SCREEN = "\033[H\033[2J";
@@ -24,15 +25,15 @@ public class CLIUtils {
         return ANSI_UNDERLINE + s + ANSI_RESET;
     }
 
+    public static String makeItalic(String s) {
+        return ANSI_ITALIC + s + ANSI_RESET;
+    }
+
     public static String color(String s, CLIColor color) {
         return color + s + ANSI_RESET;
     }
 
-    public static String tilePickableNext(TileType tileType) {
-        return makeBold(tileNotPickable(tileType));
-    }
-
-    public static String tileNotPickable(TileType tileType) {
+    private static String tileColor(TileType tileType) {
         return switch (tileType) {
             case BOOK -> color(tileType.toString(), CLIColor.ANSI_WHITE);
             case CAT -> color(tileType.toString(), CLIColor.ANSI_GREEN);
@@ -42,6 +43,14 @@ public class CLIUtils {
             case TROPHY -> color(tileType.toString(), CLIColor.ANSI_CYAN);
             default -> "";
         };
+    }
+
+    public static String tilePickableNext(TileType tileType) {
+        return makeUnderlined(makeBold(tileColor(tileType)));
+    }
+
+    public static String tileNotPickable(TileType tileType) {
+        return makeItalic(tileColor(tileType));
     }
 
     public static String tilePickable(TileType tileType) {
