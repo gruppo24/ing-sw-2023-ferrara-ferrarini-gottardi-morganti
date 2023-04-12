d131b76f845f5d1c1455705324681d5c5d581af4
Andrea Ferrarini <andrea4.ferrarini@mail.polimi.it>
2023-04-12 15:28:00 +0200

    CLI-1.0 completed (game fully playable)

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/CLI.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/CLI.java
@@ -2,7 +2,9 @@ package it.polimi.ingsw.client.view.cli;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 import it.polimi.ingsw.client.controller.Connection;
@@ -33,7 +35,7 @@ public class CLI {
      */
     Scanner in = new Scanner(System.in);
 
-    public CLI() throws UnknownHostException, IOException {
+    public CLI() throws IOException {
         System.out.println("Select server (1 - Socket, 2 - jRMI):");
         char choice = in.next().charAt(0);
         if (choice == '1') {
@@ -48,23 +50,23 @@ public class CLI {
     /**
      * Pre-game menu, allows the user to create a new game or connect to an existing
      * one
-     * 
-     * @throws IOException
      */
-    public void menu() throws IOException {
+    public void menu() {
         while (true) {
             System.out.println("\n\033[1mMyShelfie - Gruppo 24\033[0m");
             System.out.println("1. Create a new game");
             System.out.println("2. List existing games");
-            System.out.println("3. Rejoin a game");
-            System.out.println("4. Exit");
+            System.out.println("3. Join a game");
+            System.out.println("4. Rejoin a game");
+            System.out.println("5. Exit");
 
             char choice = in.next().charAt(0);
             switch (choice) {
                 case '1' -> createNewGame();
                 case '2' -> listGames();
-                case '3' -> rejoinGame();
-                case '4' -> {
+                case '3' -> joinGame(false);
+                case '4' -> joinGame(true);
+                case '5' -> {
                     System.out.println("Exiting...");
                     System.exit(0);
                 }
@@ -79,44 +81,22 @@ public class CLI {
     /**
      * Prints out all the current games on the server and allows the user to join
      * one
-     * 
-     * @throws IOException
      */
-    private void listGames() throws IOException {
+    private void listGames() {
         System.out.println("\n\033[1mExisting games:\033[0m");
         Map<String, int[]> games = this.connection.getAvailableGames();
-        String[] gameIDs = games.keySet().toArray(new String[0]);
-        if (gameIDs.length == 0) {
+
+        if (games.keySet().size() == 0)
             System.out.println("No games available");
-            menu();
-        } else {
-            for (int i = 0; i < gameIDs.length; i++) {
-                System.out.println((i + 1) + ")" + gameIDs[i] + " " + games.get(gameIDs[i])[0] + "/"
-                        + games.get(gameIDs[i])[1] + " players");
-            }
-            System.out.println("Enter the number of the game you want to join: ");
-            int choice = in.nextInt();
-            System.out.println("Enter your username: ");
-            String username = in.next();
-
-            System.out.println("Connecting to game " + gameIDs[choice - 1] + "...");
-
-            ResponseStatus res = this.connection.connectToGame(gameIDs[choice - 1], username, false);
-            if (res == ResponseStatus.SUCCESS) {
-                this.myUsername = username;
-                this.game();
-            } else {
-                System.out.println(res);
-            }
-        }
+        else
+            for (String gameID : games.keySet())
+                System.out.println(" - " + gameID + " (" + games.get(gameID)[0] + "/" + games.get(gameID)[1] +")");
     }
 
     /**
      * Create a new game, asking the num of players and the username
-     * 
-     * @throws IOException
      */
-    public void createNewGame() throws IOException {
+    private void createNewGame() {
         System.out.println("Enter the number of players: ");
         int numPlayers = in.nextInt();
         System.out.println("Enter your username: ");
@@ -133,13 +113,16 @@ public class CLI {
         }
     }
 
-    public void rejoinGame() throws IOException {
+    /**
+     * Joins a game or rejoins an existing game session after a disconnection
+     */
+    private void joinGame(boolean joining) {
         System.out.println("Enter game ID: ");
         String gameID = in.next();
         System.out.println("Enter your username: ");
         String username = in.next();
 
-        ResponseStatus res = this.connection.connectToGame(gameID, username, true);
+        ResponseStatus res = this.connection.connectToGame(gameID, username, joining);
         if (res == ResponseStatus.SUCCESS) {
             this.myUsername = username;
             this.game();
@@ -151,62 +134,52 @@ public class CLI {
     /**
      * Main game loop, waits for the server to send the game state and prints it out
      */
-    public void game() {
+    private void game() {
         // As soon as we enter the game loop, we check whether it is our turn
         SharedGameState gameState = this.connection.waitTurn();
 
+        int myIndex;
         while (!gameState.gameOver) {
+            // We clear the terminal before printing anything
+            CLIUtils.clearScreen();
+
             // We print the game state ONLY if the game has started
-            CLIUtils.clearScreen();  // We clear the terminal before printing anything
             if (gameState.gameOngoing) this.printSharedGameState(gameState);
             else System.out.println("Game not started... waiting for players");
-            gameState = this.connection.waitTurn();
 
-            /*
-            // Loop a long as it isn't the player's turn OR the game hasn't started
-            while (!gameState.players[gameState.currPlayerIndex].equals(this.myUsername) || !gameState.gameOngoing) {
-                System.out.println("=== NOT MY TURN ===");
-                gameState = this.connection.waitTurn();
-            }
-
-            System.out.println("=== MY TURN ===");
-            this.printBoard(gameState.boardContent);
-
-            System.out.println("Choose column:");
-            int column = this.in.nextInt();
-            this.connection.selectColumn(column);
-
-            // FIXME: LIMITING PICK TO ONE TILE AT THE TIME...
-            System.out.println("Choose tile (x first, then y):");
-            int x = this.in.nextInt();
-            int y = this.in.nextInt();
-            while (gameState.boardState[x][y] != TileState.PICKABLE) {
-                System.out.println("INVALID CHOICE!!");
-                x = this.in.nextInt();
-                y = this.in.nextInt();
-            }
-            this.connection.pickTile(x, y);
-
-            // FIXME: REORDER IS STATIC
-            gameState = this.connection.reorder(0, 1, 2);
-            this.printBoard(gameState.boardContent);
-             */
-            }
-
-            System.out.println("GAME IS OVER");
+            // Checking if the game has started OR if our turn hasn't come yet
+            myIndex = Arrays.asList(gameState.players).indexOf(this.myUsername);
+            gameState = (!gameState.gameOngoing || myIndex != gameState.currPlayerIndex) ?
+                    this.connection.waitTurn() :
+                    handleTurn(gameState);
         }
+
+        System.out.println("GAME IS OVER");
+    }
 
     /**
      * Method in charge of rendering a SharedGameState for the CLI interface
      * @param game SharedGameState instance to render
      */
-    public void printSharedGameState(SharedGameState game) {
+    private void printSharedGameState(SharedGameState game) {
         int myIndex = 0;
         for (String player : game.players)
             if (player.equals(this.myUsername))
                 break;
             else
                 myIndex++;
+
+        // Printing whether this is the final round of the game
+        if (game.isFinalRound)
+            System.out.println(CLIUtils.makeBold(CLIUtils.color(
+                    CLIUtils.color("FINAL ROUND OF THE GAME", CLIColor.ANSI_BACKGROUND_RED)
+                    , CLIColor.ANSI_WHITE))
+            );
+        else
+            System.out.println();
+
+        // Printing whether the player is the first player of the turn
+        System.out.println();
 
         // At first, we print our own library along with
         // our selectionBuffer and selectedColumn, if they are
@@ -287,10 +260,12 @@ public class CLI {
 
                 if (player.length() > 2 * game.libraries[myIndex].length)   // Trimming player username if necessary
                     player = player.substring(0, 2 * game.libraries[myIndex].length) + "...";
+                if (playerIndex == game.armchairIndex)  // Checking if the player is the first player
+                    player = player + "(0)";
                 int padding = 2*game.libraries[myIndex].length - player.length();
 
                 if (playerIndex == game.currPlayerIndex)    // If it's the players turn, we make their username bold
-                    player = CLIUtils.makeBold(player);
+                    player = CLIUtils.makeUnderlined(CLIUtils.makeBold(player));
 
                 System.out.print(player);
                 System.out.print(" ".repeat(padding + 8));
@@ -300,6 +275,11 @@ public class CLI {
 
         // Finally, printing the board
         printBoard(game.boardContent, game.boardState);
+
+        // Before returning, we print the current common and private objectives
+        for (String commonDesc : game.commonsDesc)
+            System.out.println(CLIUtils.makeBold("Common objective: ") + commonDesc);
+        System.out.println(CLIUtils.makeBold("Your private objective: ") + game.privateDesc);
     }
 
     /**
@@ -331,4 +311,56 @@ public class CLI {
         for (int i = 0; i < boardContent[0].length; i++) System.out.print(i + " ");
         System.out.println();
     }
+
+    /**
+     * Method in charge of handling user interactions whenever it is their turn
+     * @param game latest SharedGameState instance
+     * @return instance of SharedGameState after user interaction
+     */
+    private SharedGameState handleTurn(SharedGameState game) {
+        // If it actually is the player's turn, we, at first,
+        // check they have selected a column
+        if (game.selectionBuffer == null) {
+            System.out.print("Select a column > ");
+            return connection.selectColumn(in.nextInt());
+        }
+
+        // Checking if it is possible to pick tiles
+        if (this.areThereTilesPickable(game.boardState) && game.selectionBuffer[game.selectionBuffer.length-1] == null) {
+            System.out.print("Pick a tile - tile x coordinate (-1 if done):");
+            int x = in.nextInt();
+            System.out.print("Pick a tile - tile y coordinate (also -1 if done):");
+            int y = in.nextInt();
+
+            // Checking if the player has actually picked a tile
+            if (x != -1 || y != -1) return this.connection.pickTile(x, y);
+        }
+
+        // Checking if some tiles have actually been selected...
+        if (game.selectionBuffer[0] != null) {
+            int[] r = {0, 1, 2};
+            for (int index = 0; index < Arrays.stream(game.selectionBuffer).filter(Objects::nonNull).count(); index++) {
+                System.out.println("Index of tile number " + index + " to insert in library");
+                r[index] = in.nextInt();
+            }
+            return connection.reorder(r[0], r[1], r[2]);
+        }
+
+        // If no action has been performed, we simply return the same SharedGameState instance
+        return game;
+    }
+
+    /**
+     * This method is in charge of checking whether any tile is pickable withing a given board
+     * @param boardState board to check
+     * @return whether any tile is in state TileState.PICKABLE
+     */
+    private boolean areThereTilesPickable(TileState[][] boardState) {
+        for (TileState[] column : boardState)
+            for (TileState tile : column)
+                if (tile == TileState.PICKABLE)
+                    return true;
+        return false;
+    }
+
 }
