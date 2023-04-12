b8d1460faaa0fc8bbf1150f78f9f3f944d415028
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-04-12 11:52:49 +0200

    changed colors for tiles depending on state

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/CLIUtils.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/cli/CLIUtils.java
@@ -7,12 +7,17 @@ public class CLIUtils {
     private static final String ANSI_RESET = "\u001B[0m";
 
     private static final String ANSI_BOLD = "\u001B[1m";
+    private static final String ANSI_FAINT = "\u001B[2m";
     private static final String ANSI_UNDERLINE = "\u001B[4m";
 
     private static final String ANSI_CLEAR_SCREEN = "\033[H\033[2J";
 
     public static String makeBold(String s) {
         return ANSI_BOLD + s + ANSI_RESET;
+    }
+
+    public static String makeFaint(String s) {
+        return ANSI_FAINT + s + ANSI_RESET;
     }
 
     public static String makeUnderlined(String s) {
@@ -23,29 +28,32 @@ public class CLIUtils {
         return color + s + ANSI_RESET;
     }
 
-    public static void printTileType(TileType tileType) {
-        switch (tileType) {
-            case BOOK:
-                System.out.print(color(tileType.toString(), CLIColor.ANSI_BACKGROUND_WHITE));
-                break;
-            case CAT:
-                System.out.print(color(tileType.toString(), CLIColor.ANSI_BACKGROUND_GREEN));
-                break;
-            case FRAME:
-                System.out.print(color(tileType.toString(), CLIColor.ANSI_BACKGROUND_BLUE));
-                break;
-            case PLANT:
-                System.out.print(color(tileType.toString(), CLIColor.ANSI_BACKGROUND_PURPLE));
-                break;
-            case TOY:
-                System.out.print(color(tileType.toString(), CLIColor.ANSI_BACKGROUND_YELLOW));
-                break;
-            case TROPHY:
-                System.out.print(color(tileType.toString(), CLIColor.ANSI_BACKGROUND_CYAN));
-                break;
-            default:
-                break;
-        }
+    public static String tilePickableNext(TileType tileType) {
+        return switch (tileType) {
+            case BOOK -> color(tileType.toString(), CLIColor.ANSI_WHITE);
+            case CAT -> color(tileType.toString(), CLIColor.ANSI_GREEN);
+            case FRAME -> color(tileType.toString(), CLIColor.ANSI_BLUE);
+            case PLANT -> color(tileType.toString(), CLIColor.ANSI_PURPLE);
+            case TOY -> color(tileType.toString(), CLIColor.ANSI_YELLOW);
+            case TROPHY -> color(tileType.toString(), CLIColor.ANSI_CYAN);
+            default -> "";
+        };
+    }
+
+    public static String tileNotPickable(TileType tileType) {
+        return makeFaint(tilePickableNext(tileType));
+    }
+
+    public static String tilePickable(TileType tileType) {
+        return switch (tileType) {
+            case BOOK -> color(tileType.toString(), CLIColor.ANSI_BACKGROUND_WHITE);
+            case CAT -> color(tileType.toString(), CLIColor.ANSI_BACKGROUND_GREEN);
+            case FRAME -> color(tileType.toString(), CLIColor.ANSI_BACKGROUND_BLUE);
+            case PLANT -> color(tileType.toString(), CLIColor.ANSI_BACKGROUND_PURPLE);
+            case TOY -> color(tileType.toString(), CLIColor.ANSI_BACKGROUND_YELLOW);
+            case TROPHY -> color(tileType.toString(), CLIColor.ANSI_BACKGROUND_CYAN);
+            default -> "";
+        };
     }
 
     public static void clearScreen() {
