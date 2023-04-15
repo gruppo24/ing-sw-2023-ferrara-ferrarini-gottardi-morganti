ca17ed01dc6e67e8bae5fa3a5b03776519bf3efd
Silvia <silvia.ferrara@mail.polimi.it>
2023-04-15 17:45:43 +0200

    rename of JRMI server controller classes

--- MyShelfie/src/main/java/it/polimi/ingsw/server/controller/JRMI/preGameStubImpl.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/server/controller/JRMI/preGameStubImpl.java
@@ -1,14 +1,20 @@
 package it.polimi.ingsw.server.controller.JRMI;
 
+import it.polimi.ingsw.common.messages.responses.GamesList;
 import it.polimi.ingsw.common.messages.responses.ResponseStatus;
+import it.polimi.ingsw.server.model.GameState;
+import it.polimi.ingsw.server.model.Player;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 import java.util.Map;
 
+import static it.polimi.ingsw.server.Server.GAMES;
+import static it.polimi.ingsw.server.controller.socket.TCPPregameChannel.sendEmptyMessage;
 
-public class PreGameStubImpl extends UnicastRemoteObject implements PreGameStub{
-    public PreGameStubImpl() throws RemoteException {
+public class preGameStubImpl extends UnicastRemoteObject implements preGameStub{
+    public preGameStubImpl() throws RemoteException {
         super();
     }
 
